cc485d8c09ebe1dbe2eabda849f6c1c719b07972
Paul-Kijtapart <scc385@gmail.com>
2017-01-24 01:07:37 -0800

    Sync QuestionList with question properly

--- client/src/components/QA.js
+++ client/src/components/QA.js
@@ -2,9 +2,30 @@ import React from 'react';
 
 // A question posted by a user/admin
 class Question extends React.Component {
+	constructor(props) {
+		super(props);
+		this.upvote = this.upvote.bind(this);
+	}
+
+	// Upvote this question
+	upvote(event) {
+		event.preventDefault();
+	}
+
 	render() {
+		const questionDetail = this.props.questionDetail;
+		const name = this.props.name;
+		const timeStamp = this.props.timeStamp;
 		return (
 			<li className="Question">
+				<button
+					onChange={this.upvote}
+				> 
+				Like 
+				</button>
+				<span> {questionDetail} </span>
+				<span> {name} </span>
+				<span> {timeStamp} </span>
 			</li>
 		);
 	}
@@ -13,8 +34,22 @@ class Question extends React.Component {
 // Displays List of Active Questions 
 class QuestionList extends React.Component {
 	render() {
+		var activeQuestions = this.props.activeQuestions;
+
+		// Order questions by its Rating
+		activeQuestions.sort(function(q1, q2) {
+			if (q1.rating > q2.rating) {
+				return 1;
+			} else if (q1.rating < q2.rating) {
+				return -1;
+			} else {
+				return 0;
+			}
+		});
+
 		return (
 			<ul className="QuestionList">
+				{activeQuestions}
 			</ul>
 		);
 	}
@@ -24,7 +59,7 @@ class QuestionForm extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			question: "",
+			questionDetail: "",
 			name: ""
 		};
 
@@ -36,11 +71,23 @@ class QuestionForm extends React.Component {
 	// Add the newly posted question to the List of Active questions
 	handleSubmit(event) {
 		event.preventDefault();
+		const timeStamp = new Date().toLocaleString();
+		const numQuestions = this.props.numQuestions;
+		const question =
+			(<Question
+				key={numQuestions}
+				id={numQuestions}
+				questionDetail = {this.state.questionDetail}
+				name = {this.state.name}
+				timeStamp = {timeStamp}
+				rating={0}
+			/>);
+		this.props.addQuestion(question);
 	}
 
 	handleQuestionChange(event) {
 		this.setState({
-			question: event.target.value
+			questionDetail: event.target.value
 		});
 	}
 
@@ -57,7 +104,7 @@ class QuestionForm extends React.Component {
 			<form onSubmit={this.handleSubmit}>
 				<input
 					onChange={this.handleQuestionChange}
-					value={this.state.question} 
+					value={this.state.questionDetail} 
 					type="text"
 					placeholder="Ask anything"
 				/>
@@ -90,16 +137,22 @@ class QA extends React.Component {
 			let activeQuestions = prevState.activeQuestions.slice();
 			activeQuestions.push(question);
 			return {
-				activeQuestions: question
+				activeQuestions: activeQuestions
 			}
 		});
 	}
 
 	render() {
+		const numQuestions = this.state.activeQuestions.length;
 		return (
 			<div className="QA">
-				<QuestionForm />
-				<QuestionList />
+				<QuestionForm 
+					addQuestion = {this.addQuestion}
+					numQuestions = {numQuestions}
+				/>
+				<QuestionList 
+					activeQuestions = {this.state.activeQuestions}
+				/>
 			</div>
 		);
 	}
